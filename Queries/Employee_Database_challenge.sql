79c9d9b663d910bc3bbaf7782af34fcd968361cb
toainn92 <69770689+toainn92@users.noreply.github.com>
2020-11-08 18:48:43 -0800

    Complete Challenge 7

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,3 +30,11 @@ ORDER BY cnt DESC;
 --Table of total number of counts of retired employees based on title
 SELECT * FROM retiring_titles
 
+--Deliverable 2: The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees as e INNER JOIN
+dept_emp as de ON (e.emp_no=de.emp_no) INNER JOIN
+titles as t ON (e.emp_no=t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date='9999-01-01')
+ORDER BY e.emp_no;
